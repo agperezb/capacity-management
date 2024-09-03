4133f5c56178a6634aec74aa7f5ac86682718446
Angel Pérez <angel.perez@pragma.com.co>
2024-09-03 11:45:51 -0500

    first commit

---
+++ src/main/java/org/agperezb/capacitymanagement/CapacityManagementApplication.java
@@ -0,0 +1,13 @@
+package org.agperezb.capacitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapacityManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapacityManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/agperezb/capacitymanagement/CapacityManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.agperezb.capacitymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapacityManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
